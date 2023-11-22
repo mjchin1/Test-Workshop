e3800861fc9089c3770792d5b6a03aae9e23568d
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-11-22 11:45:07 -0600

    edited js file.

--- index.js
+++ index.js
@@ -23,12 +23,15 @@
 // Functional Tests
 
 // Shopping Cart 
-// - When the user presses the "Checkout" button, check to make sure that there are more than 0 items in the cart. 
-// - If there are 0 items in the cart, prompt the user to add items to the cart. 
-// - If there are 1 or more items in the cart, prompt the user to choose whether they would like to log in to an      account or check out as a guest. 
-//      - If the user chooses to check out as a guest: ask for name, address, phone number and email address. 
-//      - If the user chooses to log in: populate name, address, phone number and email address with stored        information.  
-// - Prompt user with a "Continue to Checkout" button. 
-// - When user clicks "Continue to Checkout" button, take them to a new page that lists the users information, the items in the cart and the subtotal for the items. 
-// - Use the user's zip code to determine tax and shipping costs. 
+
+// - When the user presses the "Checkout" button, app checks to make sure that there are more than 0 items in the cart. 
+// - If there are 0 items in the cart, app prompts the user to add items to the cart. 
+// - If there are 1 or more items in the cart, app prompts the user to choose whether they would like to log in to an      account or check out as a guest. 
+//      - If the user chooses to check out as a guest, app leads them to a page with a form that asks for name, address, phone number and email address inputs. 
+//      - If the user chooses to log in, app takes them to a page that populates a form with name, address, phone number and email address data from the database's stored information.  
+// - If any fields are missing, app prompts user to complete all fields before making "Continue to Checkout" button available. 
+// - Once all fields are complete, app allows user to click "Continue to Checkout" button.
+// - While taking user to next page, app calculates tax and shipping costs using the user's zip code.  
+// - When user clicks "Continue to Checkout" button, app takes them to a new page that lists the user's information, the items in the cart, the subtotal for the items, the tax and shipping costs, and the final total. 
+// - When user clicks "Confirm Order,"  all order/user information is sent to database.  
  
