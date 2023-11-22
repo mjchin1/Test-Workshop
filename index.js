218832c11fe8e310e362c5e96c08683234a3e94b
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-11-22 11:28:53 -0600

    added updated JS file.

---
+++ index.js
@@ -0,0 +1,34 @@
+// Unit Tests 
+
+// function multiplication(x,y)
+
+// - Expect multiplication(6,4) to accept two numbers as inputs 
+// - Expect multiplication(6,4) to return a number 
+// - Expect multiplication(6,4) to be equal to 24 
+// - Expect multiplication(6,0) to be equal to 0
+// - Expect multiplication(6,1) to be equal to 6  
+// - Expect multiplication("six",4) to return an error
+
+//function concatOdds()
+
+// - Expect concatOdds() to accept two arrays as inputs 
+// - Expect concatOdds() to return a single array 
+// - Expect concatOdds() to return an array that contains only odd numbers
+// - Expect concatOdds() to return an array that includes any elements that are repeated in the inputs only once
+// - Expect concatOdds() to return an empty array if no odd numbers are provided in the inputs
+// - Expect concatOdds() to return an empty array if only strings are provided in the inputs
+// - Expect concatOdds() to return an array that is ordered from lowest value to highest value 
+
+
+// Functional Tests
+
+// Shopping Cart 
+// - When the user presses the "Checkout" button, check to make sure that there are more than 0 items in the cart. 
+// - If there are 0 items in the cart, prompt the user to add items to the cart. 
+// - If there are 1 or more items in the cart, prompt the user to choose whether they would like to log in to an      account or check out as a guest. 
+//      - If the user chooses to check out as a guest: ask for name, address, phone number and email address. 
+//      - If the user chooses to log in: populate name, address, phone number and email address with stored        information.  
+// - Prompt user with a "Continue to Checkout" button. 
+// - When user clicks "Continue to Checkout" button, take them to a new page that lists the users information, the items in the cart and the subtotal for the items. 
+// - Use the user's zip code to determine tax and shipping costs. 
+ 
